74f6a5c7338e3ae1383663e796d3df13e068ae57
Curtis Thompson <curtiswaynethompson@gmail.com>
2024-06-09 19:26:55 +0100

    Change model to guess number of goals

--- src/build_model.py
+++ src/build_model.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from xgboost import XGBClassifier
+from xgboost import XGBRegressor
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 def cross_validate_model(x, y, model):
     tscv = TimeSeriesSplit(n_splits=5, test_size=5000)
 
-    acc, prec, rec, f1 = 0, 0, 0, 0
+    mse = 0
+    #acc, prec, rec, f1 = 0, 0, 0, 0
 
     for k, (train_index, test_index) in enumerate(tscv.split(x)):
         # Split dataset
@@ -21,16 +22,18 @@ def cross_validate_model(x, y, model):
 
         # Evaluate model
         y_pred = model_cv.predict(x_test)
-        acc += accuracy_score(y_test, y_pred)
-        prec += precision_score(y_test, y_pred)
-        rec += recall_score(y_test, y_pred)
-        f1 += f1_score(y_test, y_pred)
+        #acc += accuracy_score(y_test, y_pred)
+        #prec += precision_score(y_test, y_pred)
+        #rec += recall_score(y_test, y_pred)
+        #f1 += f1_score(y_test, y_pred)
+        mse += mean_squared_error(y_test, y_pred)
 
-    acc = acc / 5
-    prec = prec / 5
-    rec = rec / 5
-    f1 = f1 / 5
-    return acc, prec, rec, f1
+    #acc = acc / 5
+    #prec = prec / 5
+    #rec = rec / 5
+    #f1 = f1 / 5
+    mse /= 5
+    return mse
 
 
 # Load data
@@ -40,13 +43,14 @@ df = pd.read_csv('./data/etl/features.csv')
 columns = ['Neutral', 'IsHome', 'IsMajorTournament', 'IsFriendly', 'IsEuros', 'Year',
            'Recent3', 'Recent5', 'Recent10', 'EloA', 'EloB']
 df_x = df[columns]
-df_y = df.Points == 3
+df_y = df['ScoreA']
 
 # Cross validate and fit model
-model  = XGBClassifier()
-acc, prec, rec, f1 = cross_validate_model(df_x, df_y, model)
-print(f'Accuracy: {round(acc, 5)}, Precision: {round(prec, 5)}, Recall: {round(rec, 5)}, F1 Score: {round(f1, 5)}')
-model = XGBClassifier().fit(df_x, df_y)
+model  = XGBRegressor()
+mse = cross_validate_model(df_x, df_y, model)
+rmse = sqrt(mse)
+print(f'Mean Squared Error: {round(rmse, 5)}')
+model = XGBRegressor().fit(df_x, df_y)
 
 # Test prediction
 #df_pred = pd.DataFrame([[1, 0, 1, 0, 1, 2024, 3, 3, 3, 1600, 1500]], columns=columns)
