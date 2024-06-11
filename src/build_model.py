9d7b2e5906c9da809af410d5eb0e7ba8893f5ecc
Curtis Thompson <curtiswaynethompson@gmail.com>
2024-06-11 20:21:47 +0100

    Model predict home and away goals

--- src/build_model.py
+++ src/build_model.py
@@ -2,11 +2,13 @@ import os
 from math import sqrt
 
 import pandas as pd
-from xgboost import XGBRegressor
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import mean_squared_error
 
-def cross_validate_model(x, y, model):
+from match_model import MatchModel
+
+
+def cross_validate_model(x, ya, yb, model):
     tscv = TimeSeriesSplit(n_splits=5, test_size=5000)
 
     mse = 0
@@ -16,24 +18,18 @@ def cross_validate_model(x, y, model):
         # Split dataset
         x_train = x.loc[train_index]
         x_test = x.loc[test_index]
-        y_train = y[train_index]
-        y_test = y[test_index]
+        ya_train = ya[train_index]
+        ya_test = ya[test_index]
+        yb_train = yb[train_index]
+        yb_test = yb[test_index]
 
         # Train model
-        model_cv = model.fit(x_train, y_train)
+        model_cv = model.fit(x_train, ya_train, yb_train)
 
         # Evaluate model
-        y_pred = model_cv.predict(x_test)
-        #acc += accuracy_score(y_test, y_pred)
-        #prec += precision_score(y_test, y_pred)
-        #rec += recall_score(y_test, y_pred)
-        #f1 += f1_score(y_test, y_pred)
-        mse += mean_squared_error(y_test, y_pred)
+        ya_pred, yb_pred = model_cv.predict(x_test)
+        mse += (mean_squared_error(ya_test, ya_pred) + mean_squared_error(yb_test, yb_pred)) / 2
 
-    #acc = acc / 5
-    #prec = prec / 5
-    #rec = rec / 5
-    #f1 = f1 / 5
     mse /= 5
     return mse
 
@@ -42,17 +38,18 @@ def cross_validate_model(x, y, model):
 df = pd.read_csv('./data/etl/features.csv')
 
 # Choose features
-columns = ['Neutral', 'IsHome', 'IsMajorTournament', 'IsFriendly', 'IsEuros', 'Year',
-           'Recent3', 'Recent5', 'Recent10', 'EloA', 'EloB']
+columns = ['Neutral', 'IsHomeA', 'IsHomeB', 'IsMajorTournament', 'IsFriendly', 'IsEuros', 'Year',
+           'Recent3A', 'Recent5A', 'Recent10A', 'Recent3B', 'Recent5B', 'Recent10B', 'EloA', 'EloB']
 df_x = df[columns]
-df_y = df['ScoreA']
+df_ya = df['ScoreA']
+df_yb = df['ScoreB']
 
 # Cross validate and fit model
-model  = XGBRegressor()
-mse = cross_validate_model(df_x, df_y, model)
+model = MatchModel()
+mse = cross_validate_model(df_x, df_ya, df_yb, model)
 rmse = sqrt(mse)
 print(f'Mean Squared Error: {round(rmse, 5)}')
-model = XGBRegressor().fit(df_x, df_y)
+model = MatchModel().fit(df_x, df_ya, df_yb)
 
 # Save model
 os.makedirs('./data/models/', exist_ok=True)
---
+++ src/match_model.py
@@ -0,0 +1,35 @@
+import pickle
+from sklearn.base import BaseEstimator
+import numpy as np
+from xgboost import XGBRegressor
+
+
+class MatchModel(BaseEstimator):
+
+    def __init__(self, **kwargs):
+        self.model_a = XGBRegressor(**kwargs)
+        self.model_b = XGBRegressor(**kwargs)
+    
+
+    def fit(self, x, ya, yb):
+        # Fit models
+        self.model_a = self.model_a.fit(x, ya)
+        self.model_b = self.model_b.fit(x, yb)
+
+        return self
+    
+
+    def predict(self, x):
+        # Get predictions
+        pred_a = np.round(self.model_a.predict(x)).astype(int)
+        pred_b = np.round(self.model_b.predict(x)).astype(int)
+
+        return [pred_a, pred_b]
+    
+
+    def save_model(self, path):
+        pickle.dump(self, open(path, 'wb'))
+    
+
+    def load_model(self, path):
+        pickle.load(self, open(path, 'rb'))
